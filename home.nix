d95989fba1d7a52215258123f61d2f3ca8a71c3b
jahan <jahan.pd@gmail.com>
2024-11-28 11:46:30 +1100

    changed user

--- home.nix
+++ home.nix
@@ -1,10 +1,10 @@
 { pkgs, config, lib, ... }: {
 		imports = [ <home-manager/nix-darwin> ];
-		users.users.eve = {
+		users.users.jahan = {
 			name = "jahan";
 			home = "/Users/jahan";
 		};
-		home-manager.users.eve = { pkgs, ... }: {
+		home-manager.users.jahan = { pkgs, ... }: {
 			home.packages = [ 
 			  pkgs.bat 
 				pkgs.fzf
@@ -12,25 +12,23 @@
 			programs.zsh = {
 					enable = true;
 					enableCompletion = true;
-					autosuggestions.enable = true;
 					syntaxHighlighting.enable = true;
 
 					shellAliases = {
 					};
 					history = {
 						size = 10000;
-						path = "${config.xdg.dataHome}/zsh/history";
 					};
 					oh-my-zsh = {
 						enable = true;
 						plugins = [ "git" "thefuck" "fasd" "tmux" "vi-mode" "fzf"];
-						theme = "robbyrussell";
+						theme = "sonicradish";
 					};
 
 				};
 
 			# The state version is required and should stay at the version you
 			# originally installed.
-			home.stateVersion = "24.05";
+			home.stateVersion = "24.11";
 		};
 }
