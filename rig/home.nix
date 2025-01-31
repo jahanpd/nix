b9d41f48da283f92ab7f9b8a065ab30b1a99bd4c
jahizzle <me@jcpd.xyz>
2025-01-31 14:50:15 +1100

    minor changes

--- rig/home.nix
+++ rig/home.nix
@@ -18,5 +18,6 @@
   programs.zsh.shellAliases = {
 					nupdate = "sudo nixos-rebuild switch --flake ~/nix#nixos";
 					rollthedice = ''swww img "$(find /home/jahan/.config/hypr/wallpapers/ -type f | shuf -n 1)"'';
+					addssd = "sudo mount 4a3f54f6-e7e6-4b53-94b0-8600e914a8ed -U /mnt/sketchy";
 			};
 }
