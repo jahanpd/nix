1c8af78560a9d1a19c7e063185e0ea7a75a59c7a
jahan <jahan.pd@gmail.com>
2026-02-24 23:24:17 +1100

    added some gnarly shieeet

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   # rig SSH host key — run `cat /etc/ssh/ssh_host_ed25519_key.pub` on the rig
-  rig = "ssh-ed25519 AAAA_REPLACE_WITH_RIG_HOST_KEY";
+  # rig = "ssh-ed25519 AAAA_REPLACE_WITH_RIG_HOST_KEY";
+	rig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIzAmne+srsRkLcpez9C0NWRexRW95ZCXRwte3KWZ73 root@nixos";
 in
 {
-  "cloudflared-rig-token.age".publicKeys = [ rig ];
+  "cloudflared-rig-ssh-token.age".publicKeys = [ rig ];
 }
