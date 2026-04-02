5d9def83c1f36f80895a37e23ad0768e09170bc8
jahan <jahan.pd@gmail.com>
2026-04-03 10:36:31 +1100

    some changes

--- rig/home.nix
+++ rig/home.nix
@@ -12,7 +12,7 @@
     pkgs.waybar
     pkgs.rofi
 		pkgs.alacritty
-		pkgs.swww
+		pkgs.awww
 		pkgs.gh
 		pkgs.swayidle
 		pkgs.ffmpeg
@@ -24,8 +24,12 @@
   ];
   programs.zsh.shellAliases = {
 					nupdate = "sudo nixos-rebuild switch --flake ~/nix#nixos";
-					rollthedice = ''swww img "$(find /home/jahan/.config/hypr/wallpapers/ -type f | shuf -n 1)"'';
+					rollthedice = ''awww img "$(find /home/jahan/.config/hypr/wallpapers/ -type f | shuf -n 1)"'';
 					addssd = "sudo mount 4a3f54f6-e7e6-4b53-94b0-8600e914a8ed -U /mnt/sketchy";
 					magictrack = "bluetoothctl connect 3C:50:02:BF:9E:00";
 			};
+  home.sessionVariables = {
+    NPM_CONFIG_PREFIX = "${config.home.homeDirectory}/.npm-global";
+    PATH = "${config.home.homeDirectory}/.npm-global/bin:$PATH";
+  };
 }
