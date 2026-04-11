f85d9f02c289c83becdebbff6e56e37751bae1e2
jahan <jahan.pd@gmail.com>
2026-04-11 13:01:12 +1000

    changes to system key for mac

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   # rig SSH host key — run `cat /etc/ssh/ssh_host_ed25519_key.pub` on the rig
 	rig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIzAmne+srsRkLcpez9C0NWRexRW95ZCXRwte3KWZ73 root@nixos";
-	mac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILG7+2Q8+DBxfiCYPdJY+q+gA/wULeshWhMXMD+WVQP2";
+	mac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNij9A4SfU86mfYUkyRqngR3tCnD4egHl1hQCtJJO5z";
 in
 {
   "cloudflared-rig-token.age".publicKeys = [ rig ];
