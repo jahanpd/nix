2885f136efca0f6611425470e551a1f8253996fe
jahan <jahan.pd@gmail.com>
2026-05-24 14:11:14 +1000

    some king of channge

--- rig/configuration.nix
+++ rig/configuration.nix
@@ -57,7 +57,17 @@
   # potentially critical for running dynamic binaries eg bundled cuda
 	programs.nix-ld.enable = true;
 
+	services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn;
+	networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+
+services.resolved = {
+  enable = true;
+  dnssec = "true";
+  domains = [ "~." ];
+  fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  dnsovertls = "true";
+};
 
   # Enable sound.
   # hardware.pulseaudio.enable = true;
